3d782eb313773180ae7d4b3927dbf39cdc354e1f
julianolorenzato <julianolorenzato@gmail.com>
2026-09-20 14:18:15 -0300

    fomat: bvol.cpp

--- relief/src/op/bvol.cpp
+++ relief/src/op/bvol.cpp
@@ -4,38 +4,47 @@
 
 namespace op::bvol {
 
-void BoundingVolumeOp::buildBoxMesh(mesh::Mesh& mesh,
-                                     const Eigen::Vector3d& center,
-                                     const std::array<Eigen::Vector3d, 3>& axes,
-                                     const Eigen::Vector3d& halfExtents) {
+void BoundingVolumeOp::apply(mesh::Mesh& mesh) const {
+    if (mesh.vertices.empty()) return;
+    switch (type_) {
+        case BoundingVolumeType::AABB:
+            applyAABB(mesh);
+            break;
+        case BoundingVolumeType::OBB:
+            applyOBB(mesh);
+            break;
+    }
+}
+
+void BoundingVolumeOp::buildBoxMesh(mesh::Mesh& mesh, const Eigen::Vector3d& center,
+                                    const std::array<Eigen::Vector3d, 3>& axes,
+                                    const Eigen::Vector3d& halfExtents) {
     static const double signs[8][3] = {
-        {-1,-1,-1}, {+1,-1,-1}, {+1,+1,-1}, {-1,+1,-1},
-        {-1,-1,+1}, {+1,-1,+1}, {+1,+1,+1}, {-1,+1,+1},
+        {-1, -1, -1}, {+1, -1, -1}, {+1, +1, -1}, {-1, +1, -1},
+        {-1, -1, +1}, {+1, -1, +1}, {+1, +1, +1}, {-1, +1, +1},
     };
     static const int tris[12][3] = {
-        {0,3,2}, {0,2,1},   // bottom  (z = -hz)
-        {4,5,6}, {4,6,7},   // top     (z = +hz)
-        {0,1,5}, {0,5,4},   // front   (y = -hy)
-        {3,7,6}, {3,6,2},   // back    (y = +hy)
-        {0,4,7}, {0,7,3},   // left    (x = -hx)
-        {1,2,6}, {1,6,5},   // right   (x = +hx)
+        {0, 3, 2}, {0, 2, 1},  // bottom  (z = -hz)
+        {4, 5, 6}, {4, 6, 7},  // top     (z = +hz)
+        {0, 1, 5}, {0, 5, 4},  // front   (y = -hy)
+        {3, 7, 6}, {3, 6, 2},  // back    (y = +hy)
+        {0, 4, 7}, {0, 7, 3},  // left    (x = -hx)
+        {1, 2, 6}, {1, 6, 5},  // right   (x = +hx)
     };
 
     mesh.vertices.assign(8, mesh::Vertex{});
     mesh.wedges.assign(8, mesh::Wedge{});
     for (int i = 0; i < 8; i++) {
-        mesh.vertices[i].pos = center
-            + signs[i][0] * halfExtents.x() * axes[0]
-            + signs[i][1] * halfExtents.y() * axes[1]
-            + signs[i][2] * halfExtents.z() * axes[2];
+        mesh.vertices[i].pos = center + signs[i][0] * halfExtents.x() * axes[0] +
+                               signs[i][1] * halfExtents.y() * axes[1] +
+                               signs[i][2] * halfExtents.z() * axes[2];
         mesh.wedges[i].vertex = i;
         mesh.wedges[i].uv = Eigen::Vector2d::Zero();
     }
 
     mesh.faces.resize(12);
     for (int f = 0; f < 12; f++)
-        for (int c = 0; c < 3; c++)
-            mesh.faces[f].w[c] = tris[f][c];
+        for (int c = 0; c < 3; c++) mesh.faces[f].w[c] = tris[f][c];
 
     mesh.textureData.clear();
     mesh.textureWidth = 0;
@@ -43,14 +52,6 @@ void BoundingVolumeOp::buildBoxMesh(mesh::Mesh& mesh,
     mesh.normalTextureData.clear();
     mesh.normalTextureWidth = 0;
     mesh.normalTextureHeight = 0;
-}
-
-void BoundingVolumeOp::apply(mesh::Mesh& mesh) const {
-    if (mesh.vertices.empty()) return;
-    switch (type_) {
-        case BoundingVolumeType::AABB: applyAABB(mesh); break;
-        case BoundingVolumeType::OBB:  applyOBB(mesh);  break;
-    }
 }
 
 void BoundingVolumeOp::applyAABB(mesh::Mesh& mesh) const {
@@ -67,9 +68,8 @@ void BoundingVolumeOp::applyAABB(mesh::Mesh& mesh) const {
 
     Eigen::Vector3d center = 0.5 * (bmin + bmax);
     Eigen::Vector3d halfExtents = 0.5 * (bmax - bmin);
-    std::array<Eigen::Vector3d, 3> axes = {
-        Eigen::Vector3d::UnitX(), Eigen::Vector3d::UnitY(), Eigen::Vector3d::UnitZ()
-    };
+    std::array<Eigen::Vector3d, 3> axes = {Eigen::Vector3d::UnitX(), Eigen::Vector3d::UnitY(),
+                                           Eigen::Vector3d::UnitZ()};
     buildBoxMesh(mesh, center, axes, halfExtents);
 }
 
@@ -94,9 +94,8 @@ void BoundingVolumeOp::applyOBB(mesh::Mesh& mesh) const {
 
     Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> solver(cov);
     Eigen::Matrix3d evecs = solver.eigenvectors();
-    std::array<Eigen::Vector3d, 3> axes = {
-        evecs.col(0).normalized(), evecs.col(1).normalized(), evecs.col(2).normalized()
-    };
+    std::array<Eigen::Vector3d, 3> axes = {evecs.col(0).normalized(), evecs.col(1).normalized(),
+                                           evecs.col(2).normalized()};
     if (axes[0].cross(axes[1]).dot(axes[2]) < 0.0) axes[2] = -axes[2];
 
     Eigen::Vector3d bmin = Eigen::Vector3d::Constant(1e18);
@@ -111,10 +110,10 @@ void BoundingVolumeOp::applyOBB(mesh::Mesh& mesh) const {
 
     Eigen::Vector3d localCenter = 0.5 * (bmin + bmax);
     Eigen::Vector3d halfExtents = 0.5 * (bmax - bmin);
-    Eigen::Vector3d worldCenter = mean
-        + localCenter.x() * axes[0] + localCenter.y() * axes[1] + localCenter.z() * axes[2];
+    Eigen::Vector3d worldCenter =
+        mean + localCenter.x() * axes[0] + localCenter.y() * axes[1] + localCenter.z() * axes[2];
 
     buildBoxMesh(mesh, worldCenter, axes, halfExtents);
 }
 
-} // namespace op::bvol
+}  // namespace op::bvol
